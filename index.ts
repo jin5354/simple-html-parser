448c6937bacf5c8322ca3073a3c9b64768c937ec
jin5354 <xiaoyanjinx@gmail.com>
2019-01-18 18:00:19 +0800

    [feat] add test

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import {CommentNode} from './CommentNode'
 import {TextNode} from './TextNode'
 import {ElementNode} from './ElementNode'
 
-const START_TAG_REG = /^<([^>\s\/]+)((\s+[^=>\s]+(\s*=\s*((\"[^"]*\")|(\'[^']*\')|[^>\s]+))?)*)\s*\/?\s*>/m
+const START_TAG_REG = /^<([^<>\s\/]+)((\s+[^=>\s]+(\s*=\s*((\"[^"]*\")|(\'[^']*\')|[^>\s]+))?)*)\s*\/?\s*>/m
 const END_TAG_REG = /^<\/([^>\s]+)[^>]*>/m
 const ATTRIBUTE_REG = /([^=\s]+)(\s*=\s*((\"([^"]*)\")|(\'([^']*)\')|[^>\s]+))?/gm
 
@@ -12,7 +12,7 @@ interface Root {
   children: NodeList
 }
 
-module.exports = function parse(source: string) {
+export default function parse(source: string) {
 
   let result: Root = {
     children: []
@@ -37,80 +37,83 @@ module.exports = function parse(source: string) {
         console.log(`发现注释节点${source.substring(4, endIndex)}`)
         zone.children.push(new CommentNode(source.substring(4, endIndex)))
         source = source.substring(endIndex + 3)
-      }else {
-        // 若未找到，说明 <!-- 是文字内容，不是注释开头
-        charsMode = true
+        continue
       }
     }
     // 判断是不是 end Tag
-    else if(source.startsWith('</')) {
-      if(END_TAG_REG.test(source)) {
-        let left = RegExp.leftContext
-        let tag = RegExp.lastMatch
-        let right = RegExp.rightContext
+    else if(source.startsWith('</') && END_TAG_REG.test(source)) {
+      let left = RegExp.leftContext
+      let tag = RegExp.lastMatch
+      let right = RegExp.rightContext
 
-        console.log(`发现闭合标签${tag}`)
-        let result = tag.match(END_TAG_REG)
-        let name = result[1]
+      console.log(`发现闭合标签11${tag}`)
+      let result = tag.match(END_TAG_REG)
+      let name = result[1]
 
-        if(name === zone.tag) {
-          stack.pop()
-          zone = stack[stack.length - 1]
-        }else {
-          throw new Error('闭合标签对不上，html 语法出错')
-        }
-        source = right
-        return
+      if(name === zone.tag) {
+        stack.pop()
+        zone = stack[stack.length - 1]
+        console.log('闭合，出栈')
       }else {
-        // 若未找到，说明 </ 是文字内容，不是注释开头
-        charsMode = true
+        throw new Error('闭合标签对不上，html 语法出错')
       }
+      source = right
+      continue
     }
     // 判断是不是 start Tag
-    else if(source.charAt(0) === '<') {
-      if(START_TAG_REG.test(source)) {
-        let left = RegExp.leftContext
-        let tag = RegExp.lastMatch
-        let right = RegExp.rightContext
+    else if(source.charAt(0) === '<' && START_TAG_REG.test(source)) {
+      let left = RegExp.leftContext
+      let tag = RegExp.lastMatch
+      let right = RegExp.rightContext
 
-        let result = tag.match(START_TAG_REG)
-        let tagName = result[1]
-        let attrs = result[2]
-        let attrAry: Attr[] = []
-        // 抽取 attributes
-        if(attrs) {
-          attrs.replace(ATTRIBUTE_REG, (a0, a1, a2, a3, a4, a5, a6) => {
-            let attrName = a1
-            let attrValue = a3
-            attrAry.push({
-              name: attrName,
-              value: attrValue || null
-            })
+      let result = tag.match(START_TAG_REG)
+      let tagName = result[1]
+      let attrs = result[2]
+      let attrAry: Attr[] = []
+      // 抽取 attributes
+      if(attrs) {
+        attrs.replace(ATTRIBUTE_REG, (a0, a1, a2, a3, a4, a5, a6) => {
+          let attrName = a1
+          let attrValue = a3
+          attrAry.push({
+            name: attrName,
+            value: attrValue || null
           })
-        }
-
-        console.log(`发现元素节点${tag}`)
-        let element = new ElementNode(tagName, attrAry, [])
-        zone.children.push(element)
-        // 如果不是自闭合 tag，入栈
-        if(!tag.endsWith('/>')) {
-          stack.push(element)
-          zone = element
-        }
-        source = right
-      }else {
-        // 若未找到，说明 < 是文字内容，不是注释开头
-        charsMode = true
+        })
       }
+
+      console.log(`发现元素节点${tag}`)
+      let element = new ElementNode(tagName, attrAry, [])
+      zone.children.push(element)
+      // 如果不是自闭合 tag，入栈
+      if(!tag.endsWith('/>')) {
+        stack.push(element)
+        zone = element
+      }
+      source = right
+      continue
     }
 
-    if(charsMode) {
-      let index = source.indexOf('<')
-      if(index == -1) {
+    // 确认为文字模式，开始识别文本节点
+    console.log('开始识别文字')
+    let index = source.indexOf('<', 1)
+    if(index == -1) {
+      if(zone.children[zone.children.length - 1] instanceof TextNode) {
+        zone.children[zone.children.length - 1].content += source
+      }else {
         zone.children.push(new TextNode(source))
       }
+      source = ''
+    }else {
+      if(zone.children[zone.children.length - 1] instanceof TextNode) {
+        zone.children[zone.children.length - 1].content += source.substring(0, index)
+      }else {
+        zone.children.push(new TextNode(source.substring(0, index)))
+      }
+      source = source.substring(index)
     }
   }
 
-  return result
+  console.log('result:', JSON.stringify(result))
+  return result.children
 }
---
+++ test/test.ts
@@ -0,0 +1,44 @@
+import test from 'ava'
+import parse from '../index'
+import {ElementNode} from '../ElementNode'
+
+test('basic nested', t => {
+
+  let testHTML = `<div id="a" b=c d>
+    <!-- 测试注释节点 -->
+    <span>123<<<<</span>6666<<<<<
+  </div>`
+
+  let ast = parse(testHTML)
+
+  let result = [{
+    tag: 'div',
+    attrs: [{
+      name: 'id',
+      value: '"a"'
+    }, {
+      name: 'b',
+      value: 'c'
+    }, {
+      name: 'd',
+      value: null
+    }],
+    children: [{
+      content: '\n    ',
+    }, {
+      content: ' 测试注释节点 '
+    }, {
+      content: '\n    '
+    }, {
+      tag: 'span',
+      attrs: [],
+      children: [{
+        content: '123<<<<',
+      }]
+    }, {
+      content: '6666<<<<<\n  '
+    }]
+  }]
+  t.deepEqual(JSON.stringify(ast), JSON.stringify(result))
+})
+
